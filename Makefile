d8417969be99b02e9240b8bbdadfb5f949112669
Shivan Modha <shivanmodha@gmail.com>
2019-11-22 09:54:01 -0500

    Better Testing

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 all:
 	@echo "This does nothing for now"
 proc: multitest_proc.o
-	@gcc -o searchtest searchtest.c multitest_proc.o
+	@gcc -o searchtest searchtest.c multitest_proc.o -lm
 multitest_proc.o: multitest_proc.c
 	@gcc -o multitest_proc.o -c multitest_proc.c
 thread: multitest_thread.o
-	@gcc -o searchtest searchtest.c multitest_thread.o -lpthread
+	@gcc -o searchtest searchtest.c multitest_thread.o -lpthread -lm
 sequential: multitest_sequential.o
-	@gcc -o searchtest searchtest.c multitest_sequential.o
+	@gcc -o searchtest searchtest.c multitest_sequential.o -lm
 multitest_sequential.o: multitest_sequential.c
 	@gcc -o multitest_sequential.o -c multitest_sequential.c
 multitest_thread.o: multitest_thread.c
--- searchtest.c
+++ searchtest.c
@@ -5,29 +5,62 @@
  */
 // macros
 #define MAX_SPLIT_SIZE 250
-#define SIZE 20000
-#define SPLIT 1000
+#define SIZE 1000
+#define SPLIT 10
 
 // necessary imports
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <math.h>
 #include "multitest.h"
+
+typedef struct batch_results {
+    long total;
+    long average;
+    long min;
+    long max;
+    long stdev;
+} BatchResults;
 
 // function definitions
 void print(int* _array, int _size); // Print an array
 void scramble(int* _array, int _size); // Scramble an array
 int* generate(int _size); // Create a sequential array of size
 int srch(int* _array, int _size, int _value, int _splitSize); // Wrapper to the search function, to verify the split size doesn't exceed MAX_SPLIT_SIZE
-
+BatchResults execute(int* _array, int _size, int _interval); // Execute a singular test unit
 // test function definitions
-void test_verify(int* _array, int _size, int _splitSize);
+int test_verify(int* _array, int _size, int _splitSize);
 
 /**
  * application entry point
  */
 int main(int argc, char** argv) {
-    int* list = generate(SIZE);
-    test_verify(list, SIZE, SPLIT);
+    printf("*******************************************************\n");
+    printf("Search Testing Application\n");
+    printf("\tMode: %s\n", getMode());
+    printf("*******************************************************\n");
+    printf("\n");
+    printf("Test 1: Functionality Verification\n");
+    printf("    --(n=1000, ns=100)-->\n");
+    int* list = generate(1000);
+    int success = test_verify(list, 1000, 100);
+    if (success != 0) {
+        exit(1);
+    }
+    printf("\n*******************************************************\n\n");
+    printf("Test 2: Trend Analysis\n");
+    printf("n\tns\ttotal\taverage\tmin\tmax\tstdev\n");
+    int i = 10;
+    for (i = 10; i < 1001; i += 10) {
+        scramble(list, i);
+        BatchResults br = execute(list, i, 100);
+        if (br.total < 0) {
+            i -= 10;
+        } else {
+            printf("%d\t%d\t%li\t%li\t%li\t%li\t%li\n", i, SPLIT, br.total, br.average, br.min, br.max, br.stdev);
+        }
+    }
     return 0;
 }
 
@@ -77,17 +110,59 @@ int srch(int* _array, int _size, int _value, int _splitSize) {
         return search(_array, _size, _value, _splitSize);
     }
 }
+/**
+ * define a singular test run
+ */
+BatchResults execute(int* _array, int _size, int _interval) {
+    struct timeval tv_start, tv_end;
+    gettimeofday(&tv_start, NULL);
+    int i = 0;
+    BatchResults br;
+    br.average = 0;
+    br.min = 10000000;
+    br.max = 0;
+    br.stdev = 0;
+    int* ptr = malloc(sizeof(int) * _interval);
+    for (i = 0; i < _interval; i++) {
+        int index = rand() % _size;
+        struct timeval t_start, t_end;
+        gettimeofday(&t_start, NULL);
+        srch(_array, _size, _array[index], SPLIT);
+        gettimeofday(&t_end, NULL);
+        ptr[i] = (t_end.tv_usec - t_start.tv_usec);
+        br.average += ptr[i];
+        if (ptr[i] < br.min) {
+            br.min = ptr[i];
+        }
+        if (ptr[i] > br.max) {
+            br.max = ptr[i];
+        }
+    }
+    br.average /= _interval;
+    gettimeofday(&tv_end, NULL);
+    br.total = (tv_end.tv_usec - tv_start.tv_usec);
+    for (i = 0; i < _interval; i++) {
+        br.stdev += (ptr[i] - br.average) * (ptr[i] - br.average);
+    }
+    if (_interval > 1) {
+        br.stdev /= (_interval - 1);
+        br.stdev = (long)sqrt((double)br.stdev);
+    } else {
+        br.stdev = 0;
+    }
+    return br;
+}
 
-void test_verify(int* _array, int _size, int _splitSize) {
+int test_verify(int* _array, int _size, int _splitSize) {
     scramble(_array, _size);
     int i = 0;
     for (i = 0; i < _size; i++) {
-        int index = srch(_array, _size, _array[i], 250);
+        int index = srch(_array, _size, _array[i], _splitSize);
         if (i != index) {
-            printf("Verification Failed: Expected Index %d, Received Index %d. Searched for %d.\n", i, index, _array[i]);
-            break;
-        } else {
-            printf("Pass: %d\n", i);
+            printf("        Error: Expected %d, but received %d.\n", i, index);
+            return -1;
         }
     }
+    printf("        Pass! Stress tests can begin!\n");
+    return 0;
 }
